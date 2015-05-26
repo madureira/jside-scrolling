58ea4caf86ad226eb3022be14901e7e1e9fbbd23
Rafael Madureira dos Santos <rafael.santos2@walmart.com>
2015-05-26 09:49:25 -0300

    Added test to concepts

---
+++ public/test.js
@@ -0,0 +1,73 @@
+var Test = {};
+
+var IMG_PATH = '../bin/img/parallax';
+
+Test.init = function() {
+    Test.insertCanvas();
+    var imgW = 720;
+    var imgH = 500;
+
+    var $canvas = document.getElementById('viewport');
+    var canvasWidth = $canvas.width;
+    var canvasHeight = $canvas.height;
+
+    var context = $canvas.getContext('2d');
+
+    var startPosX = 0;
+
+
+    var image  = new Image();
+
+    image.onload = function() {
+        Test.draw(context, image, startPosX, imgW, canvasWidth, canvasHeight);
+    };
+
+    image.src = IMG_PATH + '/choco.jpg';
+
+};
+
+Test.draw = function(context, image, startPosX, imgW, canvasWidth, canvasHeight) {
+
+    var FPS = 1000/30;
+
+    var secondPosX;
+
+    setInterval(function() {
+        context.clearRect(0,0,canvasWidth, canvasHeight);
+
+        context.drawImage(image, startPosX, 0);
+
+        secondPosX = imgW + startPosX;
+
+        if (startPosX < canvasWidth) {
+            context.drawImage(image, secondPosX, 0);
+
+            if (Math.abs(startPosX) > canvasWidth) {
+                startPosX = secondPosX - secondPosX;
+            }
+
+            //startPosX = secondPosX;
+        }
+
+        startPosX -= 5;
+    }, FPS);
+};
+
+
+Test.insertCanvas = function() {
+    var canvas = document.createElement('canvas');
+    canvas.id = 'viewport';
+    canvas.width = 800;
+    canvas.height = 300;
+    canvas.style.border = '1px solid red';
+
+    document.getElementById('stage').appendChild(canvas);
+};
+
+
+var isReady = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(isReady);
+        Test.init();
+    }
+}, 50);
