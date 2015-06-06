348e5db4bf66c72f75ff3f602084f485fe8f7b04
Rafael Madureira dos Santos <rafael.santos2@walmart.com>
2015-06-06 02:40:49 -0300

    Tests with gamepad

--- src/javascript/main/engine/input/Gamepad.js
+++ src/javascript/main/engine/input/Gamepad.js
@@ -42,30 +42,28 @@ Game.define('Gamepad', 'engine/input', (function(fn, undefined) {
 
     fn.prototype.init = function() {
         console.log('GAMEPAD');
-        _mappingButtons()
+        _mappingButtons(this);
     };
 
-    function _mappingButtons() {
+    function _mappingButtons(self) {
         var gp = _getGamepad();
-
-        var $pad = document.getElementById('pad');
 
         setInterval(function() {
             for(var i=0; i < gp.axes.length; i++) {
                 if (gp.axes[1] === AXES.UP) {
-                    this.up = true;
-                    this.down = false;
+                    self.up = true;
+                    self.down = false;
                 } else if (gp.axes[1] === AXES.DOWN) {
-                    this.down = true;
-                    this.up = false;
+                    self.down = true;
+                    self.up = false;
                 }
 
                 if (gp.axes[0] === AXES.RIGHT) {
-                    this.right = true;
-                    this.left = false;
+                    self.right = true;
+                    self.left = false;
                 } else if (gp.axes[1] === AXES.LEFT) {
-                    this.left = true;
-                    this.right = false;
+                    self.left = true;
+                    self.right = false;
                 }
             }
         }, 100);
