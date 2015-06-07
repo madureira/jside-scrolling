848db4888990fec93a3b7ae32e467ba2cf06f732
Rafael Madureira dos Santos <rafael.santos2@walmart.com>
2015-06-06 22:46:31 -0300

    Testes with draw lineTo

--- public/test2.js
+++ public/test2.js
@@ -17,16 +17,31 @@ Test.init = function() {
         context.drawImage(image, -20, 0);
 
         context.beginPath();
+
         context.strokeStyle = '#ff0000';
+        //context.strokeStyle = 'rgba(0,0,0,0.5)';
+        //context.fillStyle = 'rgba(0,0,0,0.5)';
+        context.lineWidth = 10;
+
         context.moveTo(0, 280);
         context.lineTo(50,280);
         context.lineTo(115,245);
         context.lineTo(150,280);
         context.lineTo(165,280);
         context.lineTo(165,250);
-        context.lineTo(190,250);
-        //context.bezierCurveTo(140, 10, 388, 10, 388, 170);
-        context.lineWidth = 10;
+        context.lineTo(185,250);
+        context.lineTo(185,228);
+        context.lineTo(215,228);
+        context.lineTo(215,280);
+        context.lineTo(260,280);
+        context.lineTo(260,250);
+        context.lineTo(285,250);
+        context.lineTo(285,280);
+        context.lineTo(310,280);
+        context.bezierCurveTo(500, 210, 330, 320, 580, 250);
+        context.lineTo(630,280);
+        context.lineTo(650,280);
+
         context.stroke();
     };
     image.src = IMG_PATH + '/level.jpg';
