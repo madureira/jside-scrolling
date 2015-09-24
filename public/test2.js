02eae759759e4b433e868c91225cf20500948ed3
Rafael Madureira dos Santos <rafael.santos2@walmart.com>
2015-09-24 13:25:48 -0300

    Added mario to test collision with line

--- public/test2.js
+++ public/test2.js
@@ -3,9 +3,9 @@ var Test = {};
 var IMG_PATH = '../bin/images/tests'
 
 var player = {
-    x: 0,
+    x: 10,
     y: 235,
-    width: 20,
+    width: 40,
     height: 40,
     speed: 3,
     velX: 0,
@@ -26,49 +26,69 @@ Test.init = function() {
     var context = $canvas.getContext('2d');
 
 
-    var image  = new Image();
-    image.onload = function() {
-        context.drawImage(image, -20, 0);
+    var level = new Image();
+    level.onload = function() {
+        context.drawImage(level, -20, 0);
 
+    };
+    level.src = IMG_PATH + '/level.jpg';
+
+
+    var mario = new Image();
+    mario.onload = function() {
+        context.drawImage(mario, player.x, player.y, player.width, player.height);
         context.beginPath();
-
-        context.strokeStyle = '#ff0000';
-        context.lineWidth = 10;
-
-        context.moveTo(0, 280);
-        context.lineTo(50,280);
-        context.lineTo(115,245);
-        context.lineTo(150,280);
-        context.lineTo(165,280);
-        context.lineTo(165,250);
-        context.lineTo(185,250);
-        context.lineTo(185,228);
-        context.lineTo(215,228);
-        context.lineTo(215,280);
-        context.lineTo(260,280);
-        context.lineTo(260,250);
-        context.lineTo(285,250);
-        context.lineTo(285,280);
-        context.lineTo(310,280);
-        context.bezierCurveTo(500, 210, 330, 320, 580, 250);
-        context.lineTo(630,280);
-        context.lineTo(650,280);
-
-        context.stroke();
-        context.closePath();
-
-
-        context.beginPath();
-        context.fillStyle = "blue";
-        context.fillRect(player.x, player.y, player.width, player.height);
         context.closePath();
     };
+    mario.src = IMG_PATH + '/mario.png';
 
-    image.src = IMG_PATH + '/level.jpg';
 
+    Test.renderLoop(canvasWidth, canvasHeight, context, level, mario);
 
     Test.control();
 
+};
+
+Test.renderLoop = function(cw, ch, context, level, mario) {
+
+    setInterval(function() {
+
+        context.clearRect(0, 0, cw, ch);
+        context.drawImage(level, -20, 0);
+        Test.insertGuideLine(context);
+        context.drawImage(mario, player.x, player.y, player.width, player.height);
+
+    }, 1000 / 30);
+
+};
+
+Test.insertGuideLine = function(context) {
+    context.beginPath();
+
+    context.strokeStyle = 'blue';
+    context.lineWidth = 10;
+
+    context.moveTo(0, 280);
+    context.lineTo(50,280);
+    context.lineTo(115,245);
+    context.lineTo(150,280);
+    context.lineTo(165,280);
+    context.lineTo(165,250);
+    context.lineTo(185,250);
+    context.lineTo(185,228);
+    context.lineTo(215,228);
+    context.lineTo(215,280);
+    context.lineTo(260,280);
+    context.lineTo(260,250);
+    context.lineTo(285,250);
+    context.lineTo(285,280);
+    context.lineTo(310,280);
+    context.bezierCurveTo(500, 210, 330, 320, 580, 250);
+    context.lineTo(630,280);
+    context.lineTo(650,280);
+
+    context.stroke();
+    context.closePath();
 };
 
 Test.control = function() {
@@ -85,7 +105,6 @@ Test.control = function() {
         switch (e.keyCode) {
             case KEYBOARD.UP[0]:
             case KEYBOARD.UP[1]:
-                self.up = true;
                 break;
 
             case KEYBOARD.DOWN[0]:
@@ -95,12 +114,12 @@ Test.control = function() {
 
             case KEYBOARD.RIGHT[0]:
             case KEYBOARD.RIGHT[1]:
-                self.right = true;
+                player.x += 5;
                 break;
 
             case KEYBOARD.LEFT[0]:
             case KEYBOARD.LEFT[1]:
-                self.left = true;
+                player.x -= 5;
                 break;
         };
     };
